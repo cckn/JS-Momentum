374ad33213543534132b4cedbd96ccd0a432db69
ds <sdsd>
2019-07-15 07:26:00 +0900

    날씨 조회를 위한 위치 값 얻어오기

---
+++ weather.js
@@ -0,0 +1,43 @@
+const COORDS = "coords";
+
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj))
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.longitude;
+  const longitude = position.coords.longitude;
+
+  const coordsObj = {
+    latitude,
+    longitude
+  };
+  saveCoords(coordsObj)
+}
+
+function handleGeoError() {
+  console.log("geo Error");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+  console.log("ask");
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    console.log("ddd?");
+
+    //getWeather
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
